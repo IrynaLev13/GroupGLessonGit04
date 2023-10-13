969bb3c966e55b52a20ec30e792a78273c02e4d6
IrynaLev13 <ikot@ukr.net>
2023-10-13 16:48:25 +0300

    Add System.out.println

--- src/HW04GitLevchenko.java
+++ src/HW04GitLevchenko.java
@@ -1,6 +1,7 @@
 public class HW04GitLevchenko {
     public static void main (String[] args){
         System.out.println("Hello world and Git! I am Iryna");
+        System.out.println("Second commit for Git");
 
 
     }
