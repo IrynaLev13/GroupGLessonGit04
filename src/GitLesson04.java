ac93dabf8061ceb653fef7c63cc88118ad61dae9
IrynaLev13 <ikot@ukr.net>
2023-10-13 13:14:43 +0300

    Add new System.out.println

--- src/GitLesson04.java
+++ src/GitLesson04.java
@@ -2,5 +2,7 @@ public class GitLesson04 {
 
     public static void main (String[] args){
         System.out.println("Hello world and Git! I am Iryna");
+        System.out.println("Second commit for Git");
+
     }
 }
